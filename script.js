14a76f5425c2f2601b26f3f6ff285794c7b68f17
Stuart King <146850539+stuartking10@users.noreply.github.com>
2024-04-28 21:42:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,87 @@
-var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Sports/Ultra%20Trail%20Races.csv"
-//these are all of the colomn variables we need for our app
-var Country = getColumn(url,4);
-var Distance = getColumn(url,5);
-var Elevation = getColumn(url,6);
-// var event = getColumn (url,2);
-var race = getColumn(url,2);
-
-// this function is what gets and prints our results
-//this is a parameterCHANGE
-function printRace(){
-   var userElevation = document.getElementById("elevation").value;
-    var userDistance =  document.getElementById("distance").value;
-    var matches = [];
-
-    console.log(matches);
-for(var i = 0; i < Elevation.length; i++){
-    if(Country[i].includes(country) && Elevation[i] < userElevation && Distance[i]<userDistance){
-        matches.push(race[i]);
-      
-
-
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/main/Science/Fast%20Food%20Nutrition.csv"
+//This can be used for utils or in script
+function getColumn(url, columnNumber){
+    var column = [];
+    var table = [];
+    var request = new XMLHttpRequest();  
+    request.open("GET", url, false);   
+    request.send(null);  
+    var csvData = new Array();
+    var jsonObject = request.responseText.split(/\r?\n|\r/);
+    for (var i = 0; i < jsonObject.length; i++) {
+      csvData.push(jsonObject[i].split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/));
     }
+    table = csvData;
+    column = getCol(table, columnNumber);
+    return column;
+  }
+  function getCol(matrix, col){
+    var column = [];
+    for(var i=1; i<matrix.length-1; i++){
+       column.push(matrix[i][col]);
+    }
+    return column;
+ }
+
+
+
+ //START CODE
+ var FoodName = getColumn(url,1);
+ var TypeName = getColumn(url,2);
+ var VenderName = getColumn(url,3);
+ var CalorieNum = getColumn(url,4);
+ var ProteinNum = getColumn(url,6);
+
+
+
+// CHANGE BELOW TO PRINT FAST FOOD RESTAURANT
+function printFood(){
+  var userProtein = document.getElementById("proteinSlider").value;
+   var userCalorie =  document.getElementById("calorieSlider").value;
+   var matches = [];
+
+   console.log(matches);
+for(var i = 0; i < CalorieNum.length; i++){
+   if(TypeName[i].includes(type) && ProteinNum[i] < userProtein && CalorieNum[i]<userCalorie){
+       matches.push(FoodName[i]);
+     
+
+
+   }
 }
 console.log(matches);
 document.getElementById("output").innerHTML = matches.join("<br><br>");
 //this creates line breaks into the results 
 }
 
-//prints 
-function distanceNum(){
-    document.getElementById("distanceShow").innerHTML = document.getElementById("distance").value;
-    console.log(document.getElementById("distance").value);
+
+  //next you will use function calls and get columns..
+
+
+  //first program requirments..
+  //defines proccedures name and return type (if necessary)
+  //contains and uses one/more paremeters that have effect on funciton of procedure
+  //implements an algorithm that includes sequencing, selection, and iteratio
+
+
+
+
+
+  //second program should show ehere student developed procedure is being called
+
+
+
+
+
+
+
+function calorienum(){
+  document.getElementById("calorieShow").innerHTML = document.getElementById("calorieSlider").value;
+  console.log(document.getElementById("calorieSlider").value);
 }
 //these two are the sliders for the app... the one on top is a slider for distance and the bottom is for elevation
-function elevationNum(){
-    document.getElementById("elevationNum").innerHTML = document.getElementById("elevation").value;
-    
+function proteinnum(){
+  document.getElementById("proteinShow").innerHTML = document.getElementById("proteinSlider").value;
+  console.log(document.getElementById("porteinSlider").value);
+  
 }
-
-
